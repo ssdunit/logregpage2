a606ee34cbe24669ac64205f521924925fb5da37
ssdunit <ssdunit456@gmail.com>
2025-09-15 23:01:20 +0530

    made few tweaks to existing code and added error class css and erroruser and errorpass css

--- linkedpages/javascript/loginscript.js
+++ linkedpages/javascript/loginscript.js
@@ -1,5 +1,6 @@
 const passwordField = document.getElementById('password');
 const showPasswordCheckbox = document.getElementById('showpassword');
+/* OLD CHECKBOX CODE
 showPasswordCheckbox.addEventListener('change', function() {
     if (this.checked) {
         passwordField.type = 'text';
@@ -7,3 +8,34 @@ showPasswordCheckbox.addEventListener('change', function() {
         passwordField.type = 'password';
     }
 });
+*/
+function delay (URL) {
+    setTimeout( function() { window.location = URL }, 2000 );
+}
+/* image as a checkbox */
+const sh = document.getElementById("showhide");
+function shp(){
+    if (sh.currentSrc.endsWith("show.png")){
+        sh.src = "../linkedpages/images/hide.png";
+        passwordField.type = 'text';
+    } else{
+        sh.src = "../linkedpages/images/show.png"
+        passwordField.type = 'password';
+    }
+}
+var inpusr = document.getElementById("email");
+var inppas = document.getElementById("password");
+function lgnbtn(){
+    if(inpusr.value == "" || inppas.value == ""){
+        if (inpusr.value == ""){
+            document.getElementById("erroruser").style.display = "block";
+            setTimeout(function(){document.getElementById("erroruser").style.display = "none"},5000);
+        } 
+        if (inppas.value == ""){
+            document.getElementById("errorpass").style.display = "block";
+            setTimeout(function(){document.getElementById("errorpass").style.display = "none"},5000);
+        } 
+    } else if(!inpusr==""&&!inppas==""){
+        document.getElementById("lgbtn").href = "../home.html"
+    }
+}
